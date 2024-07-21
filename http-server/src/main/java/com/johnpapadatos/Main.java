9261d23d47826538e204c3094fb2a868d381d75b
Papajohn77 <t8190314@aueb.gr>
2024-07-21 13:47:20 +0300

    Create a thread pool of worker threads to process the HTTP requests

--- http-server/src/main/java/com/johnpapadatos/Main.java
+++ http-server/src/main/java/com/johnpapadatos/Main.java
@@ -1,20 +1,39 @@
 package com.johnpapadatos;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-
-    private static final int DEFAULT_PORT = 8080;
+    private static final int DEFAULT_PORT = 4221;
+    private static final int DEFAULT_CORE_POOL_SIZE = 8;
+    private static final int DEFAULT_MAX_POOL_SIZE = 32;
+    private static final int DEFAULT_KEEP_ALIVE = 3000;
 
     public static void main(String[] args) {
-        BlockingQueue<Socket> queue = new LinkedBlockingQueue<>();
+        try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
+            // Ensures that the server will be able to restart without
+            // waiting for old connections to time out.
+            serverSocket.setReuseAddress(true);
 
-        DispatcherRunnable dispatcherRunnable = new DispatcherRunnable(DEFAULT_PORT, queue);
+            ExecutorService threadPool = new ThreadPoolExecutor(
+                    DEFAULT_CORE_POOL_SIZE,
+                    DEFAULT_MAX_POOL_SIZE,
+                    DEFAULT_KEEP_ALIVE,
+                    TimeUnit.MILLISECONDS,
+                    new LinkedBlockingQueue<>());
 
-        Thread dispatcherThread = new Thread(dispatcherRunnable, "Dispatcher");
-
-        dispatcherThread.start();
+            while (true) {
+                Socket socket = serverSocket.accept();
+                threadPool.submit(new WorkerRunnable(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException: " + e.getMessage());
+        }
     }
 }
---
+++ http-server/src/main/java/com/johnpapadatos/WorkerRunnable.java
@@ -0,0 +1,30 @@
+package com.johnpapadatos;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class WorkerRunnable implements Runnable {
+    private final Socket socket;
+
+    public WorkerRunnable(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream()) {
+
+            // Process the HTTP request here
+
+            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+}
